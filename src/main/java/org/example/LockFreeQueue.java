4d15143cb1bf4e7ec865e85104054e9880dba6bc
Eugene Kozlov <kozlovea8@gmail.com>
2026-06-13 17:13:38 +0300

    chore: Add LockFreeQueue implementation

---
+++ src/main/java/org/example/LockFreeQueue.java
@@ -0,0 +1,60 @@
+package org.example;
+
+import jdk.jshell.spi.ExecutionControl;
+
+import java.util.EmptyStackException;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
+
+class AtomicNode {
+    int val;
+    AtomicReference<AtomicNode> next;
+
+    AtomicNode(int val) {
+        this.val = val;
+        next = new AtomicReference<>(null);
+    }
+}
+
+class LockFreeQueue {
+    AtomicReference<AtomicNode> head;
+    AtomicReference<AtomicNode> tail;
+
+    LockFreeQueue() {
+        AtomicNode node = new AtomicNode(0);
+        head = new AtomicReference<AtomicNode>(node);
+        tail = new AtomicReference<AtomicNode>(node);
+    }
+
+    int pop() {
+        while (true) {
+            AtomicNode currHead = head.get();
+            AtomicNode result = currHead.next.get();
+
+            if (result == null) {
+                throw new EmptyStackException();
+            }
+            if (head.compareAndSet(currHead, result)) {
+                return result.val;
+            }
+        }
+    }
+
+    void push(int val) {
+        while (true) {
+            AtomicNode currTail = tail.get();
+            AtomicNode dirtyTail = currTail.next.get();
+            AtomicNode candidate = new AtomicNode(val);
+
+            if (dirtyTail != null) {
+                tail.compareAndSet(currTail, dirtyTail);
+                continue;
+            }
+
+            if (currTail.next.compareAndSet(null, candidate)) {
+                tail.compareAndSet(currTail, candidate);
+                return;
+            }
+        }
+    }
+}
